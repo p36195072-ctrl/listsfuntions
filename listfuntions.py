32066eab88187a50090788ae54245097a7242316
p36195072-ctrl <p36195072@gmail.com>
2026-05-12 00:05:38 +0530

    listsfuntions learning py

---
+++ listfuntions.py
@@ -0,0 +1,8 @@
+lucky_people = [9,45,5,6,24,53,24,64,63]
+friends = ["jon","jon","jim","kevin","sev"]
+friends2 = friends.copy()
+friends2.remove("jim")
+friends.extend (lucky_people)
+friends2 = friends.copy()
+
+print (friends2)
